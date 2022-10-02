41be8518d4cb42db57b661a7335fd6ad5167b8f7
thomasparsons <thomaspatrickparsons@gmail.com>
2022-10-02 16:06:25 +0100

    done

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,23 @@
 import React from "react";
+import "antd/dist/antd.css";
+import { Layout, Typography } from "antd";
 
 export default () => {
-  return <div>Your Next.js App</div>;
+  return (
+    <Layout.Content
+      style={{
+        backgroundColor: "#F6EEB5",
+        display: "flex",
+        flexDirection: "column",
+        height: "100vh",
+        justifyContent: "center",
+        textAlign: "center",
+        width: "100vw",
+      }}
+    >
+      <Typography.Title style={{ fontSize: "6rem", margin: "0" }}>
+        &#128161; &#129509; &#127782;
+      </Typography.Title>
+    </Layout.Content>
+  );
 };
